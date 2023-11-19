b973cd691845445fb22fdaf1559cd0f0e33cc97e
Lanpasto <77079137+Lanpasto@users.noreply.github.com>
2023-11-19 07:46:15 +0200

    Update MyAppNotice.kt

--- app/src/main/java/com/osadchiy/mysterymind/MyAppNotice.kt
+++ app/src/main/java/com/osadchiy/mysterymind/MyAppNotice.kt
@@ -2,13 +2,13 @@ package com.example.mysterymind
 
 import android.app.Application
 import com.onesignal.OneSignal
-// Replace the below with your own ONESIGNAL_APP_ID
+
 const val ONESIGNAL_APP_ID = "85d778b2-14ea-4206-81a2-ca6c85ea783f"
 class MyAppNotice : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // Logging set to help debug issues, remove before releasing your app.
+        
         OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
 
         // OneSignal Initialization
